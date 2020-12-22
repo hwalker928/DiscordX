0ffd0d8e396a166fb0968de2c2755fe1065a9e09
Jpuf0 <Jpufthebeast@gmail.com>
2020-12-22 21:50:20 +0000

    Fixed ServerCommand IP

--- src/main/java/org/harrydev/discordx/Bot/Commands/ServerCommand.java
+++ src/main/java/org/harrydev/discordx/Bot/Commands/ServerCommand.java
@@ -5,18 +5,20 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bukkit.Bukkit;
 import org.harrydev.discordx.Bot.bot;
+import org.harrydev.discordx.DiscordX;
 import org.harrydev.discordx.Utils.Lag;
 
 import java.awt.*;
 
 public class ServerCommand extends ListenerAdapter {
+    private static final DiscordX INSTANCE = DiscordX.getInstance();
 
     @Override
     public void onMessageReceived(MessageReceivedEvent e) {
         if (e.getMessage().getContentRaw().equals(bot.getPrefix() + "server")) {
             EmbedBuilder embed = new EmbedBuilder();
             embed.setTitle("Server info").setColor(Color.GREEN);
-            embed.addField("Server IP:", Bukkit.getServer().getIp(), true);
+            embed.addField("Server IP:", INSTANCE.getConfig().getString("serverIP"), true);
             embed.setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/emojidex/112/file-cabinet_1f5c4.png");
             embed.addField("Server TPS:", String.valueOf(Math.round(Lag.getTPS())), true);
             e.getChannel().sendMessage(embed.build()).queue();
