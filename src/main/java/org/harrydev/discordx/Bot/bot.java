8d473eb4bc343a783a59999bda5860f8f70d4c3d
Jpuf0 <jpuffthebeast@gmail.com>
2020-12-23 02:51:33 +0000

    Added Token and Invite Checks

--- src/main/java/org/harrydev/discordx/Bot/bot.java
+++ src/main/java/org/harrydev/discordx/Bot/bot.java
@@ -4,7 +4,6 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.TextChannel;
 import org.harrydev.discordx.Bot.Commands.HelpCommand;
 import org.harrydev.discordx.Bot.Commands.PingCommand;
 import org.harrydev.discordx.Bot.Commands.ServerCommand;
@@ -16,6 +15,7 @@ import org.harrydev.discordx.Utils.Logger;
 import javax.security.auth.login.LoginException;
 import java.awt.*;
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class bot {
 
@@ -25,25 +25,19 @@ public class bot {
     private static String prefix = INSTANCE.getConfig().getString("botPrefix");
 
     public static void Start() {
-
-        if(Token.equals("TokenGoesHere")) {
-            Logger.warn("Please modify the config.yml!");
+        if(!CheckToken(Token)) {
             return;
         }
         Logger.info("Starting the discord bot.");
         JDABuilder jdaBuilder = JDABuilder.createDefault(Token);
 
         try {
-            PingCommand pingCommand = new PingCommand();
-            ServerCommand serverCommand = new ServerCommand();
-            HelpCommand helpCommand = new HelpCommand();
-            WhitelistCommand whitelistCommand = new WhitelistCommand();
-            jdaBuilder.addEventListeners(pingCommand);
-            jdaBuilder.addEventListeners(serverCommand);
-            jdaBuilder.addEventListeners(helpCommand);
-            jdaBuilder.addEventListeners(whitelistCommand);
-            jdaBuilder.setActivity(Activity.playing("Minecraft"));
+            jdaBuilder.addEventListeners(new PingCommand());
+            jdaBuilder.addEventListeners(new ServerCommand());
+            jdaBuilder.addEventListeners(new HelpCommand());
+            jdaBuilder.addEventListeners(new WhitelistCommand());
             jdaBuilder.addEventListeners(new DiscordMessage());
+            jdaBuilder.setActivity(Activity.playing("Minecraft"));
             jda = jdaBuilder.build();
             jda.awaitReady();
             Logger.info("The bot has started!");
@@ -71,4 +65,18 @@ public class bot {
         Objects.requireNonNull(jda.getTextChannelById(INSTANCE.getConfig().getLong("chatChannel"))).sendMessage(eb.build()).queue();
     }
 
+    public static boolean CheckToken(String Token) {
+        Pattern tokenPattern = Pattern.compile("[a-zA-Z0-9\\-.]{59}");
+        if(Token.equals("TokenGoesHere")) {
+            Logger.warn("Please Set the bot token in the config.yml!");
+            Logger.warn("Aborting");
+            return false;
+        }
+        if(!tokenPattern.matcher(Token).matches()){
+            Logger.warn("Your Token is Incorrect or Malformed");
+            Logger.warn("Aborting");
+            return false;
+        }
+        return true;
+    }
 }
